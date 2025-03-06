36a54324e5cd120a45339f2f701ad7d9af1f8aa6
GabrielMalone <167805738+GabrielMalone@users.noreply.github.com>
2025-03-05 19:48:34 -0500

    fixed task three

--- week5/task_three.s
+++ week5/task_three.s
@@ -1,26 +1,29 @@
 @ ---------------------------------------------------
 @ Task 3 - Gabriel Malone 
 @ ---------------------------------------------------
+.text
+.global _start
+        _start:
 .data
-    .align 2    @ (2^2 = 4 byte alignment)
+@  removed the initial .align as I don't think it was necessary for the first byte variable
 @ ---------------------------------------------------
 @ declare variables 
-@ ---------------------------------------------------
-a:  .byte  0    @ 1 byte  0x00 assigned to 'a'
-    .align 2    @ align to next 4 bytes for upcoming word
-b:  .word  32   @ 4 bytes 0x00000020 assigned to 'b'
+@ --------------------------------------------------- boundary | padding
+a:  .byte  0    @ 1 byte  0x00 assigned to 'a'              1
+    .align 2    @ align to next 4 bytes for upcoming word   4    + 3
+b:  .word  32   @ 4 bytes 0x00000020 assigned to 'b'        8
                 @ byte next, no alignment needed
-c:  .byte  3    @ 1 byte  0x03 assigned to 'c'
-    .align 1    @ half word next 2 byte alignment needed
-d:  .hword 45   @ 2 bytes 002D assigned to 'd'
+c:  .byte  3    @ 1 byte  0x03 assigned to 'c'              9
+    .align 1    @ half word next 2 byte alignment needed    10   + 1
+d:  .hword 45   @ 2 bytes 002D assigned to 'd'              12
                 @ half word next, already aligning with hword
-e:  .hword 0    @ 2 bytes 0x0000 assigned to 'e'
+e:  .hword 0    @ 2 bytes 0x0000 assigned to 'e'            14
                 @ byte next, no alignment needed
-f:  .byte  0    @ 1 byte  0x00 assigned to 'f'
-    .align 2    @ word next 4 byte alignment needed
-g:  .word  128  @ 4 bytes 0x0080 assigned to 'g'
+f:  .byte  0    @ 1 byte  0x00 assigned to 'f'              15
+    .align 2    @ word next 4 byte alignment needed         16   + 1
+g:  .word  128  @ 4 bytes 0x0080 assigned to 'g'n           20
 @ ---------------------------------------------------
-@ 15 bytes + 10 byte align = 25 bytes
+@  15 bytes + 5 padding = 20 bytes total 
 @ ---------------------------------------------------
 
 @ ---------------------------------------------------
